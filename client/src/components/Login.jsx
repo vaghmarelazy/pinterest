9cd217ce9495dc716e78c539e38f7b9b11ec0752
Rupesh Vaghmare <100845862+vaghmarelazy@users.noreply.github.com>
2024-08-07 15:34:44 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -22,7 +22,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:3000/login", {
+      const response = await axios.post("https://pinterest-swm4.vercel.app/login", {
         username,
         password,
       });

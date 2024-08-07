5d7de863fbe7f57fe2d058f1896c90e0f5f0e36c
Rupesh Vaghmare <100845862+vaghmarelazy@users.noreply.github.com>
2024-08-07 15:36:27 +0530

    Update Profile.jsx

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -62,7 +62,7 @@ const Profile = () => {
           return;
         }
 
-        const response = await axios.get("http://localhost:3000/profile", {
+        const response = await axios.get("https://pinterest-swm4.vercel.app//profile", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
